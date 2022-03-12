d13c4ac378accebb05e0e7d1ca6b0cbcc6acc01b
Javazi <zahrarahmani.ie@gmail.com>
2022-03-12 19:09:52 +0330

    Config,Entity,Dao,Service

---
+++ src/main/java/com/example/article_management_spring/service/RoleServiceImpl.java
@@ -0,0 +1,54 @@
+package com.example.article_management_spring.service;
+
+import com.example.article_management_spring.dao.RoleDao;
+import com.example.article_management_spring.entity.Role;
+import com.example.article_management_spring.exception.RoleNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class RoleServiceImpl implements GeneralService<Role, Integer> {
+    private RoleDao roleDao;
+
+    @Autowired
+    public RoleServiceImpl(RoleDao roleDao) {
+        this.roleDao = roleDao;
+    }
+
+    @Transactional()
+    @Override
+    public List<Role> findAll() {
+        return roleDao.findAll();
+    }
+
+    @Transactional()
+    @Override
+    public Role findById(Integer id) {
+        Optional<Role> byId = roleDao.findById(id);
+        if (byId.isPresent())
+            return byId.get();
+        else throw new RoleNotFoundException("Role not found!");
+    }
+
+    @Transactional()
+    @Override
+    public Role save(Role entity) {
+        return roleDao.save(entity);
+    }
+
+    @Transactional()
+    @Override
+    public void delete(Role entity) {
+        roleDao.delete(entity);
+    }
+
+    @Transactional()
+    public Role findRoleByTitle (String title){
+        Role roleByTitle = roleDao.findRoleByTitle(title);
+        return roleByTitle;
+    }
+}
